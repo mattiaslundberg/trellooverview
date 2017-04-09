d0198071733a0714a8a0e2fec87785da0b575383
Mattias Lundberg <mattias@kundo.se>
2017-04-09 18:36:41 +0200

    Add simple tests for decoders

---
+++ tests/DecoderTests.elm
@@ -0,0 +1,50 @@
+module DecoderTests exposing (..)
+
+import Array
+import Expect
+import String
+import Test exposing (..)
+import Fuzz exposing (string)
+import Models exposing (..)
+import Views exposing (..)
+import Decoder exposing (..)
+
+
+all : Test
+all =
+    describe "Test json decoder"
+        [ test "Get board list" <|
+            \() ->
+                let
+                    json =
+                        "[{\"id\": \"someid\", \"name\": \"somename\"}]"
+                in
+                    Expect.equal
+                        [ TrelloBoard False [] "" "someid" "somename" ]
+                        (decodeBoards json)
+        , fuzz string "No bords in random string" <|
+            \s ->
+                s
+                    |> decodeBoards
+                    |> Expect.equal []
+        , test "Get List list" <|
+            \() ->
+                let
+                    json =
+                        "[{\"name\": \"somename\", \"id\": \"someid\", \"idBoard\": \"board\"}]"
+                in
+                    Expect.equal
+                        [ TrelloList [] "somename" "someid" "board" ]
+                        (decodeLists json)
+
+        -- This is the expected interface, fix decodeCards to match it
+        -- , test "Get Card list" <|
+        --     \() ->
+        --         let
+        --             json =
+        --                 "[{\"id\": \"id\", \"name\": \"name\", \"idList\": \"list\", \"idBoard\": \"board\"}]"
+        --         in
+        --             Expect.equal
+        --                 [ TrelloCard "id" "name" "list" "board" ]
+        --                 (decodeCards json)
+        ]
--- tests/Tests.elm
+++ tests/Tests.elm
@@ -3,6 +3,7 @@ module Tests exposing (..)
 import Test exposing (..)
 import CardTime
 import SummaryTime
+import DecoderTests
 
 
 all : Test
@@ -10,4 +11,5 @@ all =
     describe "Time"
         [ CardTime.all
         , SummaryTime.all
+        , DecoderTests.all
         ]

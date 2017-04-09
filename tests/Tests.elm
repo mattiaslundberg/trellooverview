3141a88aa6249ec4aa6598b7afbd30c35d094666
Mattias Lundberg <mattias@kundo.se>
2017-04-09 17:19:02 +0200

    Shorter lines in tests

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -10,7 +10,7 @@ import Views exposing (..)
 
 all : Test
 all =
-    describe "TrelloCard"
+    describe "Time"
         [ describe "Get estimation from trelloCard name"
             [ test "Get time from single number" <|
                 \() ->
@@ -53,16 +53,34 @@ all =
                 \() ->
                     let
                         board =
-                            TrelloBoard True [ TrelloList [ TrelloCard "1" "Some (5)" "1" "2", TrelloCard "" "Other (5)" "" "" ] "Version 2" "132" "123" ] ".*" "123" "123"
+                            TrelloBoard True
+                                [ TrelloList
+                                    [ TrelloCard "1" "Some (5)" "1" "2"
+                                    , TrelloCard "" "Other (5)" "" ""
+                                    ]
+                                    "Version 2"
+                                    "132"
+                                    "123"
+                                ]
+                                ".*"
+                                "123"
+                                "123"
                     in
-                        Expect.equal 10 (getBoardTimeSummary board True True)
+                        Expect.equal
+                            10
+                            (getBoardTimeSummary board True True)
             ]
         , describe "Summarize for Lists"
             [ test "Includes done list when flag is set" <|
                 \() ->
                     let
                         list =
-                            TrelloList [ TrelloCard "1" "Some (1)" "1" "2" ] "Done" "123" "123"
+                            TrelloList
+                                [ TrelloCard "1" "Some (1)" "1" "2"
+                                ]
+                                "Done"
+                                "123"
+                                "123"
                     in
                         Expect.equal 1 (getTimeFromList "Version.*" True False list)
             , test "Excludes done list when flag is not set" <|
@@ -76,15 +94,29 @@ all =
                 \() ->
                     let
                         list =
-                            TrelloList [ TrelloCard "1" "Some (1)" "1" "2" ] "Version 2" "123" "123"
+                            TrelloList
+                                [ TrelloCard "1" "Some (1)" "1" "2"
+                                ]
+                                "Version 2"
+                                "123"
+                                "123"
                     in
-                        Expect.equal 1 (getTimeFromList "Version.*" False True list)
+                        Expect.equal
+                            1
+                            (getTimeFromList "Version.*" False True list)
             , test "Excludes notdone list when flag is not set" <|
                 \() ->
                     let
                         list =
-                            TrelloList [ TrelloCard "1" "Some (1)" "1" "2" ] "Version 2" "123" "123"
+                            TrelloList
+                                [ TrelloCard "1" "Some (1)" "1" "2"
+                                ]
+                                "Version 2"
+                                "123"
+                                "123"
                     in
-                        Expect.equal 0 (getTimeFromList "Some.*" False False list)
+                        Expect.equal
+                            0
+                            (getTimeFromList "Some.*" False False list)
             ]
         ]

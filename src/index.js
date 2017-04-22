a600e0d5a122b84fa7e48822630969233b77da23
Mattias Lundberg <mattiaslundberg@openmailbox.org>
2017-04-22 19:07:18 +0200

    Auto-indent index.js

--- src/index.js
+++ src/index.js
@@ -1,91 +1,91 @@
 window.onload = function () {
-// Start TrelloOverview app
-while (document.body.firstChild) {
-  document.body.removeChild(document.body.firstChild)
-}
-var app = Elm.Main.embed(document.body)
-
-// Connect trello client js to elm application
-app.ports.trelloCheckAuthorized.subscribe(function() {
-  var isAuthorized = Trello.authorized()
-  if (isAuthorized) {
-    app.ports.trelloIsAuthorized.send(true)
-  } else {
-    app.ports.trelloIsNotAuthorized.send(true)
+  // Start TrelloOverview app
+  while (document.body.firstChild) {
+    document.body.removeChild(document.body.firstChild)
   }
-})
+  var app = Elm.Main.embed(document.body)
 
-app.ports.trelloAuthorize.subscribe(function() {
-  Trello.authorize({
-    type: 'redirect',
-    name: 'Trello Overview',
-    scope: {
-      read: 'true',
-      write: 'false'
-    },
-    expiration: 'never',
-    success: function() {
-      console.log("Success")
+  // Connect trello client js to elm application
+  app.ports.trelloCheckAuthorized.subscribe(function() {
+    var isAuthorized = Trello.authorized()
+    if (isAuthorized) {
       app.ports.trelloIsAuthorized.send(true)
-    },
-    error: function() {
-      console.log("Error")
+    } else {
+      app.ports.trelloIsNotAuthorized.send(true)
     }
   })
-})
 
-app.ports.trelloListBoards.subscribe(function() {
-  Trello.get(
-    "/members/me/boards",
-    {fields: "name, id"},
-    function(boards) {
-      app.ports.trelloBoards.send(JSON.stringify(boards))
-    },
-    function(error) {
-      console.log(error)
+  app.ports.trelloAuthorize.subscribe(function() {
+    Trello.authorize({
+      type: 'redirect',
+      name: 'Trello Overview',
+      scope: {
+        read: 'true',
+        write: 'false'
+      },
+      expiration: 'never',
+      success: function() {
+        console.log("Success")
+        app.ports.trelloIsAuthorized.send(true)
+      },
+      error: function() {
+        console.log("Error")
+      }
+    })
+  })
+
+  app.ports.trelloListBoards.subscribe(function() {
+    Trello.get(
+      "/members/me/boards",
+      {fields: "name, id"},
+      function(boards) {
+        app.ports.trelloBoards.send(JSON.stringify(boards))
+      },
+      function(error) {
+        console.log(error)
+      }
+    )
+  })
+
+  app.ports.trelloListLists.subscribe(function(id) {
+    console.log("list lists for board", id)
+    Trello.get(
+      "/boards/" + id + "/lists",
+      {},
+      function(lists) {
+        app.ports.trelloList.send(JSON.stringify(lists))
+      },
+      function(error) {
+        console.log(error)
+      }
+    )
+  })
+
+  app.ports.trelloListCards.subscribe(function(id) {
+    console.log("list cards for list", id)
+    Trello.get(
+      "/lists/" + id + "/cards",
+      {},
+      function(cards) {
+        app.ports.trelloCards.send(JSON.stringify(cards))
+      },
+      function(error) {
+        console.log(error)
+      }
+    )
+  })
+
+
+  // Connect localStorage to application
+  app.ports.localStorageSet.subscribe(function(ls) {
+    localStorage.setItem(ls.key, ls.value)
+  })
+
+  app.ports.localStorageGet.subscribe(function(key) {
+    console.log("get for key", key)
+    var value = localStorage.getItem(key)
+    if (value) {
+      app.ports.localStorageGot.send({key: key, value: value})
     }
-  )
-})
-
-app.ports.trelloListLists.subscribe(function(id) {
-  console.log("list lists for board", id)
-  Trello.get(
-    "/boards/" + id + "/lists",
-    {},
-    function(lists) {
-      app.ports.trelloList.send(JSON.stringify(lists))
-    },
-    function(error) {
-      console.log(error)
-    }
-  )
-})
-
-app.ports.trelloListCards.subscribe(function(id) {
-  console.log("list cards for list", id)
-  Trello.get(
-    "/lists/" + id + "/cards",
-    {},
-    function(cards) {
-      app.ports.trelloCards.send(JSON.stringify(cards))
-    },
-    function(error) {
-      console.log(error)
-    }
-  )
-})
-
-
-// Connect localStorage to application
-app.ports.localStorageSet.subscribe(function(ls) {
-  localStorage.setItem(ls.key, ls.value)
-})
-
-app.ports.localStorageGet.subscribe(function(key) {
-  console.log("get for key", key)
-  var value = localStorage.getItem(key)
-  if (value) {
-    app.ports.localStorageGot.send({key: key, value: value})
-  }
-})
+  })
 }

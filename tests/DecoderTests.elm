3afc44ab74604fe1f7bfac3c9815bbef30cff1bf
Mattias Lundberg <mattiaslundberg@openmailbox.org>
2017-04-23 10:10:57 +0200

    Update DecoderTests

--- tests/DecoderTests.elm
+++ tests/DecoderTests.elm
@@ -1,27 +1,33 @@
 module DecoderTests exposing (..)
 
-import Array
 import Expect
-import String
 import Test exposing (..)
-import Fuzz exposing (string)
 import Models exposing (..)
-import Views exposing (..)
 import Decoder exposing (..)
+import Json.Decode exposing (decodeString, list)
 
 
 all : Test
 all =
     describe "Test json decoder"
-        [ test "Get List list" <|
+        [ test "Get board list" <|
+            \() ->
+                let
+                    json =
+                        "[{\"id\": \"someid\", \"name\": \"somename\"}]"
+                in
+                    Expect.equal
+                        (Ok [ TrelloBoard False [] "" "someid" "somename" ])
+                        (decodeString (list boardDecoder) json)
+        , test "Get List list" <|
             \() ->
                 let
                     json =
                         "[{\"name\": \"somename\", \"id\": \"someid\", \"idBoard\": \"board\"}]"
                 in
                     Expect.equal
-                        [ TrelloList [] "somename" "someid" "board" ]
-                        (decodeLists json)
+                        (Ok [ TrelloList [] "somename" "someid" "board" ])
+                        (decodeString (list listDecoder) json)
         , test "Get Card list" <|
             \() ->
                 let
